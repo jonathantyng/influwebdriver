7c0370649015890439b2a617aa18911454d147c4
Patrick Priestley <patrick@influitive.com>
2013-10-24 15:59:26 -0400

    company create - add enable emails

--- src/test/java/test/SeleniumTest.java
+++ src/test/java/test/SeleniumTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import com.thoughtworks.selenium.*;
+import org.openqa.selenium.*;
 
 import java.util.concurrent.TimeUnit;
 
--- src/test/java/test/create_company.java
+++ src/test/java/test/create_company.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.Date;
 import java.io.File;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.interactions.Actions;
@@ -19,14 +20,14 @@ import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
 public class create_company {
-    FirefoxDriver wd;
+    ChromeDriver wd;
 
     @Before
     public void setUp() throws Exception {
 
         System.setProperty("webdriver.chrome.driver" ,"/Users/patrickprestley/Downloads/chromedriver");
 
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
@@ -40,6 +41,24 @@ public class create_company {
         wd.findElement(By.id("user_password")).clear();
         wd.findElement(By.id("user_password")).sendKeys("1nflu1t1v3");
         wd.findElement(By.id("sign-in-button")).click();
+
+        //If tutorial hasn't been completed, dismiss pop-up.. Otherwise continue.
+
+        boolean exists = wd.findElements( By.className("remove")).size() != 0;
+
+        if (exists)
+        {
+            wd.findElement(By.className("remove")).click();
+            System.out.println("Tutorial dismissed");
+        }
+        else
+        {
+            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+            System.out.println("Tutorial not encountered");
+        }
+
+        /*
+
         wd.findElement(By.xpath("//div[@id='header']/div/ul[2]/li/a/b")).click();
         wd.findElement(By.linkText("Settings")).click();
         try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
@@ -47,34 +66,45 @@ public class create_company {
         wd.findElement(By.linkText("Companies")).click();
         try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.linkText("Add a Company")).click();
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.id("company_name")).click();
         wd.findElement(By.id("company_name")).click();
         wd.findElement(By.id("company_name")).clear();
         wd.findElement(By.id("company_name")).sendKeys("oct24");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.id("company_subdomain")).click();
         wd.findElement(By.id("company_subdomain")).clear();
         wd.findElement(By.id("company_subdomain")).sendKeys("oct24");
-        wd.findElement(By.linkText("Evangelist")).click();
-        wd.findElement(By.linkText("Evangelist")).click();
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        //wd.findElement(By.linkText("Evangelist")).click();
+        //try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        //wd.findElement(By.linkText("Evangelist")).click();
         wd.findElement(By.id("company_administrator_email")).click();
         wd.findElement(By.id("company_administrator_email")).clear();
         wd.findElement(By.id("company_administrator_email")).sendKeys("samanthatester99@yahoo.com");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.id("company_administrator_name")).click();
         wd.findElement(By.id("company_administrator_name")).clear();
         wd.findElement(By.id("company_administrator_name")).sendKeys("Samantha Tester");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.linkText("Red")).click();
         wd.findElement(By.xpath("//div[@id='select2-drop']/ul/li[3]/div")).click();
         wd.findElement(By.name("commit")).click();
         try { Thread.sleep(30000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wd.get("http://oct24test.influitiveqa.com/");
+
+        */
+
+        wd.get("http://oct24.influitiveqa.com/");
         try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.linkText("Influitive Admin")).click();
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.linkText("Settings")).click();
         try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.xpath("//ul[@id='accordion']/li[3]/h4")).click();
         wd.findElement(By.linkText("Welcome Email")).click();
         try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.id("email_preferences_preferences_deliver_welcome_email")).click();
+        wd.findElement(By.name("commit")).click();
         wd.findElement(By.linkText("Invitations")).click();
         try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.linkText("Challenge Digest")).click();
@@ -86,6 +116,7 @@ public class create_company {
         wd.findElement(By.linkText("Notifications")).click();
         try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
         wd.findElement(By.id("company_preference_deliver_notifications")).click();
+        wd.findElement(By.name("commit")).click();
         wd.findElement(By.xpath("//ul[@id='accordion']/li[4]/h4")).click();
         wd.findElement(By.linkText("Enabled Features")).click();
         wd.findElement(By.id("toggles_blog_stage")).click();
@@ -94,6 +125,9 @@ public class create_company {
         wd.findElement(By.id("toggles_private_leaderboards")).click();
         wd.findElement(By.id("toggles_check_in")).click();
         wd.findElement(By.id("toggles_show_future_stages")).click();
+        wd.findElement(By.id("toggles_quick_referral")).click();
+        wd.findElement(By.id("toggles_tango_card")).click();
+        wd.findElement(By.id("toggles_referral_email")).click();
         wd.findElement(By.name("commit")).click();
     }
 
