fa46e2a2678ac24fc31ed6af124fe9560ca62c05
Patrick Priestley <patrick@influitive.com>
2013-10-31 18:00:37 -0400

    alice_sign started. Still wonky

--- src/test/java/interaction/alice_sign.java
+++ src/test/java/interaction/alice_sign.java
@@ -26,17 +26,36 @@ public class alice_sign extends BaseClassTwo {
 
     //Sign into app and access challenge screen
 
-        wd.get("http://may31.influitiveqa.com/users/sign_in");
-        wd.findElement(By.id("user_email")).click();
-        wd.findElement(By.id("user_email")).clear();
-        wd.findElement(By.id("user_email")).sendKeys("alice.advocate@gmail.com");
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wd.findElement(By.id("user_password")).click();
-        wd.findElement(By.id("user_password")).clear();
-        wd.findElement(By.id("user_password")).sendKeys("macbook18");
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wd.findElement(By.id("sign-in-button")).click();
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.get("http://may31.influitiveqa.com/join/Test");
+
+        boolean newuser = wd.findElements( By.id("registration_user_email")).size() != 0;
+
+        if (newuser)
+        {
+            wd.findElement(By.id("registration_user_email")).click();
+            wd.findElement(By.id("registration_user_email")).click();
+            wd.findElement(By.id("registration_user_email")).sendKeys("alice.advocate@gmail.com");
+            wd.findElement(By.id("registration_contact_name")).click();
+            wd.findElement(By.id("registration_contact_name")).click();
+            wd.findElement(By.id("registration_contact_name")).sendKeys("Alice Advocate");
+            wd.findElement(By.id("registration_user_password")).click();
+            wd.findElement(By.id("registration_user_password")).click();
+            wd.findElement(By.id("registration_user_password")).sendKeys("macbook18");
+            wd.findElement(By.name("commit")).click();
+        }
+        else
+        {
+            wd.findElement(By.id("user_email")).click();
+            wd.findElement(By.id("user_email")).clear();
+            wd.findElement(By.id("user_email")).sendKeys("alice.advocate@gmail.com");
+            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+            wd.findElement(By.id("user_password")).click();
+            wd.findElement(By.id("user_password")).clear();
+            wd.findElement(By.id("user_password")).sendKeys("macbook18");
+            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+            wd.findElement(By.id("sign-in-button")).click();
+            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        }
 
     //Complete tutorial
 
@@ -45,14 +64,15 @@ public class alice_sign extends BaseClassTwo {
         if (exists)
         {
             wd.findElement(By.linkText("Get Started!")).click();
-            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+            try { Thread.sleep(10000l); } catch (Exception e) { throw new RuntimeException(e); }
             wd.findElement(By.id("activity_responses_attributes_0_body_offer_my_expertise_to_help_others_when_i_can")).click();
             wd.findElement(By.name("commit")).click();
-            wd.findElement(By.linkText("Offer tickets to a popular event.")).click();
+            wd.findElement(By.id("activity_responses_attributes_0_body_offer_tickets_to_a_popular_industry_event")).click();
             wd.findElement(By.name("commit")).click();
             wd.findElement(By.id("activity_responses_attributes_0_body_an_expert_customer_who_is_willing_to_help_new_customers")).click();
             wd.findElement(By.name("commit")).click();
-            wd.findElement(By.linkText("Facebook")).click();
+            try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+            wd.findElement(By.id("activity_responses_attributes_0_body_facebook")).click();
             wd.findElement(By.name("commit")).click();
             wd.findElement(By.linkText("Let's start your next challenge")).click();
 
