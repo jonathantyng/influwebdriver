c1e8ef787d0d0c3e487eb4395bc8486cd64b2266
Patrick Priestley <patrick@influitive.com>
2014-07-08 16:59:16 -0400

    Added approval and approval feedback

---
+++ src/test/java/Advocate/Approval.java
@@ -0,0 +1,72 @@
+package Advocate;
+
+import org.junit.After;
+import org.junit.Test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class Approval extends Advocate{
+
+    @Test
+    public void approvall() {
+
+        //Sign into app and access challenge screen
+
+        wd.get("https://advocatetest.influitives.com/users/sign_in");
+        wd.findElement(By.id("user_email")).click();
+        wd.findElement(By.id("user_email")).clear();
+        wd.findElement(By.id("user_email")).sendKeys("advocatebob7@gmail.com");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.id("user_password")).click();
+        wd.findElement(By.id("user_password")).clear();
+        wd.findElement(By.id("user_password")).sendKeys("macbook18");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.id("sign-in-button")).click();
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Load challenge
+
+        wd.get("https://advocatetest.influitives.com/challenges/21");
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Enter text into field
+
+        wd.findElement(By.id("activity_responses_attributes_0_body")).click();
+        wd.findElement(By.id("activity_responses_attributes_0_body")).sendKeys("This is a response!");
+
+        //Submit response
+
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+
+        wd.findElement(By.name("commit")).click();
+
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Signout
+
+        wd.findElement(By.id("contact-dropdown")).click();
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.linkText("Sign out")).click();
+        try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+
+
+    }
+
+
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/Advocate/ApprovalFeedback.java
@@ -0,0 +1,72 @@
+package Advocate;
+
+import org.junit.After;
+import org.junit.Test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ApprovalFeedback extends Advocate{
+
+    @Test
+    public void ApprovalFeedbacks() {
+
+        //Sign into app and access challenge screen
+
+        wd.get("https://advocatetest.influitives.com/users/sign_in");
+        wd.findElement(By.id("user_email")).click();
+        wd.findElement(By.id("user_email")).clear();
+        wd.findElement(By.id("user_email")).sendKeys("advocatebob7@gmail.com");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.id("user_password")).click();
+        wd.findElement(By.id("user_password")).clear();
+        wd.findElement(By.id("user_password")).sendKeys("macbook18");
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.id("sign-in-button")).click();
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Load challenge
+
+        wd.get("https://advocatetest.influitives.com/challenges/22");
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Accept challenge
+
+        wd.findElement(By.name("commit")).click();
+
+        //Enter response
+
+        wd.findElement(By.id("activity_responses_attributes_0_body")).click();
+        wd.findElement(By.id("activity_responses_attributes_0_body")).sendKeys("Entering another response");
+
+        //Submit response
+
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.name("commit")).click();
+        try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+        //Signout
+
+        wd.findElement(By.id("contact-dropdown")).click();
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
+        wd.findElement(By.linkText("Sign out")).click();
+        try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
+
+
+    }
+
+
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
