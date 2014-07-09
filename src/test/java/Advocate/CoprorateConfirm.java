d4a75c2e1a0cf3f045a99585f64a194f73428bd4
Patrick Priestley <patrick@influitive.com>
2014-07-09 14:25:32 -0400

    Removed sign out from challenge completions

--- src/test/java/Advocate/CoprorateConfirm.java
+++ src/test/java/Advocate/CoprorateConfirm.java
@@ -41,15 +41,6 @@ public class CoprorateConfirm extends Advocate{
         wd.findElement(By.name("commit")).click();
         try { Thread.sleep(5000l); } catch (Exception e) { throw new RuntimeException(e); }
 
-        //Signout
-
-        wd.findElement(By.id("contact-dropdown")).click();
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wd.findElement(By.linkText("Sign out")).click();
-        try { Thread.sleep(3000l); } catch (Exception e) { throw new RuntimeException(e); }
-
-
-
     }
 
 
