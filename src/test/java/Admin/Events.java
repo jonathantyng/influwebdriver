8dc37e92cfaac361777d1703fe72539dd81189e0
shannonjunkin <shannon@influitive.com>
2014-10-09 12:42:46 -0400

    Updates to Events test
    admin/events

--- src/test/java/Admin/Events.java
+++ src/test/java/Admin/Events.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Test;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -25,42 +26,57 @@ public class Events extends BaseClass {
         //If tutorial hasn't been completed, dismiss pop-up.. Otherwise continue.
 
 
+        boolean exists = wd.findElements( By.className("remove")).size() != 0;
+
+        if (exists)
+        {
+            wd.findElement(By.className("remove")).click();
+            System.out.println("Challenge Tutorial Dismissed");
+        }
+        else
+
+        {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"header-bar\"]/div[1]/div/div/ul[1]/li/a")));
+            System.out.println("Tutorial not encountered");
+        }
 
         //Navigate to settings page
 
-        wd.findElement(By.cssSelector("#header-bar > div.container > div > div > ul:nth-child(1) > li > a")).click();
+        wd.findElement(By.xpath("//*[@id=\"header-bar\"]/div[1]/div/div/ul[1]/li/a")).click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#header-bar > div.container > div > div > ul:nth-child(1) > li > ul > li:nth-child(2) > a")));
-        wd.findElement(By.cssSelector("#header-bar > div.container > div > div > ul:nth-child(1) > li > ul > li:nth-child(2) > a")).click();
+        //Select Settings from dropdown
 
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Settings")));
+        wd.findElement(By.linkText("Settings")).click();
 
-        //Navigate to Events
-
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#collapse-me0 > li:nth-child(6) > a")));
-        wd.findElement(By.cssSelector("#collapse-me0 > li:nth-child(6) > a")).click();
-
-        //Add New Event
-
-        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
-        wd.findElement(By.cssSelector("#configuration-panel > div > a")).click();
-
+        //Select Events
 
         try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
 
-        //add event name
 
-        wd.findElement(By.id("zone_name")).click();
-        wd.findElement(By.id("zone_name")).sendKeys("This is a new event");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"collapse-me0\"]/li[6]/a")));
+        wd.findElement(By.xpath("//*[@id=\"collapse-me0\"]/li[6]/a")).click();
+
+        //Create new event
+
+        try { Thread.sleep(2000l); } catch (Exception e) { throw new RuntimeException(e); }
 
 
-        //add event description
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Add Event")));
+        wd.findElement(By.linkText("Add Event")).click();
 
-        wd.findElement(By.xpath("//*[@id=\"zone_description\"]")).click();
-        wd.findElement(By.xpath("//*[@id=\"zone_description\"]")).sendKeys("New events are amazing");
+        //Name/Description for New Event
 
-        wd.findElement(By.cssSelector("#new_zone > div.action-buttons.vertical-space-small > input")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("zone_name")));
+        wd.findElementById("zone_name").sendKeys("New Zone");
 
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("zone_description")));
+        wd.findElementById("zone_description").sendKeys("This is the new zone");
+
+        //Save Zone
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"new_zone\"]/div[5]/input")));
+        wd.findElement(By.xpath("//*[@id=\"new_zone\"]/div[5]/input")).click();
 
 
     }
