f0f5231ca2ee1a41555370986b00ec4d2d4f4fa8
shannonjunkin <shannon@influitive.com>
2014-12-29 14:58:35 -0500

    created DeleteGroups skeleton spec
    added new spec for attempting to delete groups.

---
+++ src/test/java/specs/admin/group/DeleteGroups.java
@@ -0,0 +1,42 @@
+
+package specs.admin.group;
+
+import org.junit.Test;
+import pageobjects.LoginPage;
+import specs.admin.AbstractSpec;
+
+public class DeleteGroups extends AbstractSpec{
+    
+    @Test
+    public void deleteEmptyGroup(){
+        LoginPage login = new LoginPage(driver);
+        //login step - unsure, we don't use the targeting module so i'm not sure what the line here would be
+        //login.loginAdministrator("admin@influitive.com", "1nflu1t1v3")
+        
+            .navigateToGroups()
+            .<pageobjects.admin.groups.List>dismissTutorial()
+            .selectDropdownOnEmptyGroup()
+            .deleteGroup();
+            
+                    
+        //verify group has been deleted        
+        
+        
+                   
+        }
+    
+    @Test
+    public void attemptDeletingGroupWithAdvocates(){
+        LoginPage login = new LoginPage(driver);
+        //login step - unsure, we don't use the targeting module so i'm not sure what the line here would be
+        //login.loginAdministrator("admin@influitive.com", "1nflu1t1v3")
+            .navigateToGroups()
+            .<pageobjects.admin.groups.List>dismissTutorial()
+            .selectDropdownOnGroupWithAdvocate()
+            .deleteGroup();
+            
+        //verify group has not been deleted
+        
+    }
+    
+}
