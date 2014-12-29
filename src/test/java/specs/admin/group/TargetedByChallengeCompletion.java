61facc087f210daac143ed9c8688f26856572245
shannonjunkin <shannon@influitive.com>
2014-12-29 13:56:36 -0500

    Created TargetedByChallengeCompletion spec steps
    TargetedByChallengeCompletion spec steps input - incomplete steps are
    commented out.

--- src/test/java/specs/admin/group/TargetedByChallengeCompletion.java
+++ src/test/java/specs/admin/group/TargetedByChallengeCompletion.java
@@ -1,15 +1,16 @@
+
 package specs.admin.group;
 
-import org.junit.Assert;
 import org.junit.Test;
 import pageobjects.LoginPage;
 import pageobjects.admin.groups.EditorTargeting;
 import specs.admin.AbstractSpec;
 
-public class TargetedByName extends AbstractSpec {
+
+public class TargetedByChallengeCompletion extends AbstractSpec{
     
     @Test
-    public void groupTargetedByNameOnlyHasThatPerson() {
+    public void groupTargetedToAdvocateCompletedChallenge () {
         LoginPage login = new LoginPage(driver);
         EditorTargeting groupEditor = login.loginAdministrator("admin@influitive.com", "1nflu1t1v3")
                 .navigateToGroups()
@@ -17,14 +18,13 @@ public class TargetedByName extends AbstractSpec {
                 .addNewGroup()
                 .generateGroupDetails()
                 .switchToTargeting()
-                .addAdvocateInfoCriteria()
-                .targetByEmail("alice.advocate@gmail.com");
-        
-        Assert.assertEquals("1", groupEditor.getMatchedAdvocateCount());
-        
-        String memberCount = groupEditor.saveGroup()
-            .waitForAndGetNewMembershipCount();
-        
-        Assert.assertEquals("1", memberCount);
-    }
+                .;
+                
+                
+                
+
+}
+    
+    
+    
 }
