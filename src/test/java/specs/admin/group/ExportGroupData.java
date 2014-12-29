71bc4682378626db4b3f1d4ab87e0179b5d1fb2d
shannonjunkin <shannon@influitive.com>
2014-12-29 14:45:19 -0500

    Updates to ExportGroupData spec
    Added skeleton steps.

--- src/test/java/specs/admin/group/ExportGroupData.java
+++ src/test/java/specs/admin/group/ExportGroupData.java
@@ -1,13 +1,20 @@
 package specs.admin.group;
 
 import org.junit.Test;
+import pageobjects.LoginPage;
 import specs.admin.AbstractSpec;
 
 public class ExportGroupData extends AbstractSpec {
     
     @Test
     public void initiateExportGroupData (){
-    
+    LoginPage login = new LoginPage(driver);
+    //login 
+                .navigateToGroups()
+                .<pageobjects.admin.groups.List>dismissTutorial()
+                .selectGroupDropdown()
+                .exportGroupData();
+                
         //verificaiton is manual - must verify report is received and correct 
     }
     
