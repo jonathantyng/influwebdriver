296e13a313ddaf0a0551868f8bfca3a7fd7708ff
shannonjunkin <shannon@influitive.com>
2015-04-06 16:11:24 -0400

    Specs for Nominee Filters
    new specs for all nominee filters

--- src/test/java/specs/admin/advocates/nominees/FilterNomineeList.java
+++ src/test/java/specs/admin/advocates/nominees/FilterNomineeList.java
@@ -17,7 +17,75 @@ public class FilterNomineeList extends AbstractSpec {
                 .filterByNomineeName();
         
     }
+  
+     @Test
+    public void filterByActiveNominees (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByActiveNominees();
+                
+        
+    }
     
+    @Test
+    public void filterByArchivedNominees (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByArchivedNominees();
+        
+    }
     
+      @Test
+    public void filterNomineeListByDetractorNPSScore (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByDetractorNominee();
+        
+    }
     
+      @Test
+    public void filterNomineeListByPromoterNPSScore (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByPromoterNominee();
+        
+    }
+    
+      @Test
+    public void filterNomineeListByNeutralNPSScore (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByNeutralNominee();
+        
+    }
+    
+      @Test
+    public void filterNomineeListByUnknownNPSScore (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByUnknownNPSNominees();
+        
+    }
+    
+      @Test
+    public void filterNomineeListBySource (){
+        LoginPage start = new LoginPage (driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .filterByNomineeSource();
+        
+    }
 }
