6fe5709420379cc8f5573b051db4dbd4875b2e1c
shannonjunkin <shannon@influitive.com>
2015-04-06 15:39:23 -0400

    Formatting
    Saved updating formatting

--- src/test/java/specs/admin/advocates/nominees/FilterNominees.java
+++ src/test/java/specs/admin/advocates/nominees/FilterNominees.java
@@ -1,73 +1,70 @@
-
 package specs.admin.advocates.nominees;
 
 import org.junit.Test;
 import pageobjects.LoginPage;
 import specs.admin.AbstractSpec;
 
+public class FilterNominees extends AbstractSpec {
 
-public class FilterNominees extends AbstractSpec{
-    
     @Test
-    public void filterNomineesByName (){
-    
+    public void filterNomineesByName() {
+
         LoginPage start = new LoginPage(driver);
         start.loginAdministrator()
                 .navigateToAdvocates()
                 .inviteNominees()
                 .sortNomineesByName();
-      
-    }
-    
-    @Test
-    public void filterNomineesBySource (){
-    
-        LoginPage start = new LoginPage(driver);
-        start.loginAdministrator()
-                .navigateToAdvocates()
-                .inviteNominees()
-                .sortNomineesBySource();   
-}
 
-        @Test
-        public void filterNomineesByNPSScore (){
-    
+    }
+
+    @Test
+    public void filterNomineesBySource() {
+
         LoginPage start = new LoginPage(driver);
         start.loginAdministrator()
                 .navigateToAdvocates()
                 .inviteNominees()
-                .sortNomineesByNPSScore();   
-}
-    
-        @Test
-        public void filterNomineesByRelationship (){
-    
+                .sortNomineesBySource();
+    }
+
+    @Test
+    public void filterNomineesByNPSScore() {
+
         LoginPage start = new LoginPage(driver);
         start.loginAdministrator()
                 .navigateToAdvocates()
                 .inviteNominees()
-                .sortNomineesByRelationship();   
-}
-            @Test
-        public void filterNomineesByChallengeCompletions (){
-    
+                .sortNomineesByNPSScore();
+    }
+
+    @Test
+    public void filterNomineesByRelationship() {
+
         LoginPage start = new LoginPage(driver);
         start.loginAdministrator()
                 .navigateToAdvocates()
                 .inviteNominees()
-                .sortNomineesByChallengeCompletions();   
+                .sortNomineesByRelationship();
+    }
+
+    @Test
+    public void filterNomineesByChallengeCompletions() {
+
+        LoginPage start = new LoginPage(driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .sortNomineesByChallengeCompletions();
+    }
+
+    @Test
+    public void filterNomineesByLastActivity() {
+
+        LoginPage start = new LoginPage(driver);
+        start.loginAdministrator()
+                .navigateToAdvocates()
+                .inviteNominees()
+                .sortNomineesByLastActivity();
+
+    }
 }
-    
-        @Test
-        public void filterNomineesByLastActivity(){
-        
-            LoginPage start = new LoginPage(driver);
-            start.loginAdministrator()
-                    .navigateToAdvocates()
-                    .inviteNominees()
-                    .sortNomineesByLastActivity();
-        
-            
-        }
-}
-    
