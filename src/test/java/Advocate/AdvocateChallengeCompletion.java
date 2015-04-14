9de83ee992beb5c3a62864fdf315e27d81107e62
Patrick Priestley <patrick@influitive.com>
2015-04-14 12:18:22 -0400

    Removing old tests that are covered with PageObjects re-write
    - Admin side navigation, and reward creation removed
    - Removed redundant Advocate completion tests
    -

--- src/test/java/Advocate/AdvocateChallengeCompletion.java
+++ src/test/java/Advocate/AdvocateChallengeCompletion.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @SuppressWarnings("unused")
 @RunWith(Suite.class)
-@SuiteClasses({BlogPost.class, FollowOnTwitter.class, JoinGroup.class, JoinLinkedinGroup.class, LinkedinRecom.class, MultiSurvey.class, NPS.class, OnlineAction.class, OnlineReview.class, Referral.class, ShareLink.class, Survey.class, TwitterPost.class})
+@SuiteClasses({BlogPost.class, FollowOnTwitter.class, JoinGroup.class, JoinLinkedinGroup.class, NPS.class, OnlineAction.class, OnlineReview.class, ShareLink.class, Survey.class, TwitterPost.class})
 public class AdvocateChallengeCompletion {
 
 }
